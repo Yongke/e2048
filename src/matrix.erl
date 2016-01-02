ab0bac771ac67ae0ba3ab71178f2069603abdd68
Yongke Wang <wangyk@rippletek.com>
2016-01-02 15:59:41 +0800

    finish move functions of the matrix

--- src/matrix.erl
+++ src/matrix.erl
@@ -1,11 +1,12 @@
 -module(matrix).
 
--export([init/0, to_list/1, from_list/1, feed/1, transpose/1]).
+-export([init/0, to_list/1, from_list/1, feed/1, transpose/1,
+         to_string/1, count_zero/1,
+         move_left/1, move_right/1, move_up/1, move_down/1]).
 
 -define(SIZE, 4).
 -define(ELEM_CNT, ?SIZE * ?SIZE).
 -define(INIT_NUMBER_CNT, 3). %% initial number is 2
-
 
 %%%===================================================================
 %%% API
@@ -20,6 +21,19 @@ init() ->
                   end
           end, lists:seq(1, ?ELEM_CNT)),
     from_list(L).
+
+to_string(M) ->
+    StrRows = lists:map(
+                fun(Row) ->
+                        lists:flatten(
+                          ["|",
+                           [string:join(
+                              [integer_to_list(Num) || Num <- Row],
+                              "||")],
+                           "|"])
+                end,
+                M),
+    string:join(StrRows, "\n").
 
 to_list(M) ->
     lists:flatten(M).
@@ -51,6 +65,38 @@ transpose([[]|_]) -> [];
 transpose(M) ->
     [lists:map(fun hd/1, M) | transpose(lists:map(fun tl/1, M))].
 
+move_left(M) ->
+    M1 = lists:map(
+           fun(Row) ->
+                   merge_row_left(Row)
+           end,
+           M),
+    assert_move(M1).
+
+move_right(M) ->
+    M1 = lists:map(
+           fun(Row) ->
+                   lists:reverse(merge_row_left(lists:reverse(Row)))
+           end,
+           M),
+    assert_move(M1).
+
+
+move_up(M) ->
+    Mt = transpose(M),
+    transpose(move_left(Mt)).
+
+move_down(M) ->
+    Mt = transpose(M),
+    transpose(move_right(Mt)).
+
+count_zero(M) ->
+    length(lists:filter(
+             fun(0) -> true;
+                (_) -> false
+             end,
+             matrix:to_list(M))).
+
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
@@ -77,4 +123,31 @@ random_aux(Len, Cnt, Acc) ->
             random_aux(Len, Cnt, Acc);
         _ ->
             random_aux(Len, Cnt - 1, [R | Acc])
+    end.
+
+
+merge_row_left(Row) ->
+    merge_row_left(
+      lists:filter(
+        fun(0) -> false;
+           (_) -> true
+        end, Row), []).
+    
+merge_row_left([], Acc) ->
+    lists:reverse(merge_padding(Acc));
+merge_row_left([H1, H2 | Tail], Acc) when H1 =:= H2 ->
+    lists:reverse(
+      merge_padding(
+        lists:reverse(Tail) ++ [H1 + H2 | Acc]));
+merge_row_left([H | Tail], Acc) ->
+    merge_row_left(Tail, [H | Acc]).
+
+merge_padding(L) ->
+    lists:duplicate(?SIZE - length(L), 0) ++ L.
+
+assert_move(M) ->
+    case count_zero(M) of
+        0 ->
+            throw(can_not_move);
+        _ -> M
     end.
--- test/matrix_test.erl
+++ test/matrix_test.erl
@@ -4,7 +4,6 @@
 
 basic_test() ->
     M = matrix:init(),
-    ?debugVal(M),
     ?assertEqual(4, length(M)),
     [ Row1 | _] = M,
     ?assertEqual(4, length(Row1)),
@@ -14,20 +13,49 @@ basic_test() ->
     ?assertEqual(M, M1),
 
     Mt = matrix:transpose(M),
-    ?debugVal(Mt),
     ?assertEqual(M, matrix:transpose(Mt)),
 
     Mf = matrix:feed(M),
-    ?debugVal(Mf),
-    
-    C = zero_cnt(M),
-    Cf = zero_cnt(Mf),
-    ?assertEqual(C - 1, Cf), 
+
+    ?assertEqual(matrix:count_zero(M) - 1, matrix:count_zero(Mf)), 
+
+    ?assertEqual("|0||0||0||0|\n|2||0||0||0|\n|0||2||2||0|\n|0||0||0||0|",
+                 matrix:to_string([[0,0,0,0],[2,0,0,0],[0,2,2,0],[0,0,0,0]])),
     ok.
 
-zero_cnt(M) ->
-    length(lists:filter(
-             fun(0) -> true;
-                (_) -> false
-             end,
-             matrix:to_list(M))).
+
+move_test() ->
+    M = [[0,0,0,0],[2,0,0,0],[0,2,2,0],[0,0,0,0]],
+    M1 = [[0,2,2,0],[2,2,0,0],[2,0,0,2],[4,4,2,4]],
+    M2 = [[8,8,8,8],[2,2,2,2],[4,4,4,4],[16,16,16,16]],
+
+    ?assertEqual([[4,0,0,0],[4,0,0,0],[4,0,0,0],[8,2,4,0]],
+                 matrix:move_left(M1)),
+    ?assertEqual([[0,0,0,0],[2,0,0,0],[4,0,0,0],[0,0,0,0]],
+                 matrix:move_left(M)),
+    ?assertEqual([[16,8,8,0],[4,2,2,0],[8,4,4,0],[32,16,16,0]],
+                 matrix:move_left(M2)),
+    
+    ?assertEqual([[0,0,0,0],[0,0,0,2],[0,0,0,4],[0,0,0,0]],
+                 matrix:move_right(M)),
+    ?assertEqual([[0,0,0,4],[0,0,0,4],[0,0,0,4],[0,8,2,4]],
+                 matrix:move_right(M1)),
+    ?assertEqual([[0,8,8,16],[0,2,2,4],[0,4,4,8],[0,16,16,32]],
+                 matrix:move_right(M2)),
+
+    ?assertEqual([[2,2,2,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+                 matrix:move_up(M)),
+    ?assertEqual([[4,4,4,2],[4,4,0,4],[0,0,0,0],[0,0,0,0]],
+                 matrix:move_up(M1)),
+    ?assertThrow(can_not_move, matrix:move_up(M2)),
+
+
+    ?assertEqual([[0,0,0,0],[0,0,0,0],[0,0,0,0],[2,2,2,0]],
+                 matrix:move_down(M)),
+    ?assertEqual([[0,0,0,0],[0,0,0,0],[4,4,0,2],[4,4,4,4]],
+                 matrix:move_down(M1)),
+    ?assertThrow(can_not_move, matrix:move_down(M2)),
+    
+    ok.
+
+
